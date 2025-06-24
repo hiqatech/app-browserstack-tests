61640cfe1e43e8360767d4d3b1f7d1ee89ef8d00
hiqatech <hiqatech@gmail.com>
2025-06-24 07:12:40 +0200

    workflow trigger

--- src/test/java/com/android/AppiumTest.java
+++ src/test/java/com/android/AppiumTest.java
@@ -5,7 +5,6 @@ import java.net.URL;
 import org.openqa.selenium.MutableCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 
